9bee16f7b2d5fee404a5778ded455b7ff2a9bd8d
mohammed mehdi <mohammedmehdisaibat@gmail.com>
2026-01-08 15:13:36 +0100

    finish findAll method to return an object of books and author for each book

--- src/Repositories/BookRepository.php
+++ src/Repositories/BookRepository.php
@@ -3,12 +3,31 @@
 declare(strict_types=1);
 
 require_once 'src/Core/Database.php';
+require_once 'src/Entities/Author.php';
+require_once 'src/Entities/Book.php';
 
 class BookRepository
 {
     public function findAll(): array
     {
         $db = Database::getInstance();
-        $sql = "SELECT "
+        $sql = "SELECT b.*, a.id as author_id, a.name as author_name, a.email as author_email FROM books b JOIN authors a on b.author_id = a.id;";
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+
+        $rows = $stmt->fetchAll();
+
+        $books = [];
+
+        foreach ($rows as $row) {
+            $author = new Author((int)$row['author_id'], $row['author_name'], $row['author_email']);
+            $book = new Book((int)$row['id'], $row['title'], (float)$row['price'], (int)$row['stock'], $author);
+            $books[] = $book;
+        }
+
+        return $books;
     }
 }
+
+$books = new BookRepository();
+print_r($books->findAll());
